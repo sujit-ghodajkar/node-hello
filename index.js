767e3a521f9afcd3b626501cbeb56dc7acb71a90
Sujit Ghodajkar <45931379+sujit-ghodajkar@users.noreply.github.com>
2024-06-17 04:13:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node from Sujit!\n'
+  const msg = 'Hello Everyone, I am Sujit!\n'
   res.end(msg);
 });
 
