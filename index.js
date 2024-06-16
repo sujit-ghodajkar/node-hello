66ad51d3cf4fe98be6d0a3382589f7b56ca98a97
Sujit Ghodajkar <45931379+sujit-ghodajkar@users.noreply.github.com>
2024-06-17 05:09:30 +0530

    Update index.js
    comment change

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Everyone, I am Sujit!\n'
+  const msg = 'Hello Everyone, I am Sujit Ghodajkar!\n'
   res.end(msg);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+  console.log(`Server is running on http://localhost:${port}/`);
 });
